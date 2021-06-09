15747c69ca69854b322465b1484d9aa59b89e771
Daniel Khrenov <klaven011@gmail.com>
2021-06-09 18:19:06 +0300

    feat: add url error messages

--- errors/errorMessages.js
+++ errors/errorMessages.js
@@ -1,25 +1,31 @@
 module.exports = {
   email: {
     'any.custom': 'Почта не прошла валидацию',
-    'any.rquired': 'Не указана почта',
+    'any.required': 'Не указана почта',
     'string.empty': 'Поле "почта" не содержит информацию',
   },
   password: {
     'string.min': 'Пароль должен содержать не менее {#limit} символов',
-    'any.rquired': 'Не указан пароль',
+    'any.required': 'Не указан пароль',
     'string.empty': 'Поле "пароль" не содержит информацию',
   },
   string: {
-    'any.rquired': 'Не указано поле {#label}',
+    'any.required': 'Не указано поле {#label}',
     'string.empty': 'Поле {#label} не содержит информацию',
-    'sting.base': 'Поле {#label} должно иметь тип sting',
+    'sting.base': 'Поле {#label} должно иметь тип string',
   },
   number:
     {
-      'any.rquired': 'Не указано поле {#label}',
+      'any.required': 'Не указано поле {#label}',
       'string.empty': 'Поле {#label} не содержит информацию',
       'number.min': '{#label} не может быть меньше 1',
       'number.base': 'Поле {#label} должно иметь тип number',
     },
+  url: {
+    'any.required': 'Не указано поле {#label}',
+    'any.custom': 'Ссылка в поле {#label} не прошла валидацию',
+    'string.empty': 'Поле {#label} не содержит информацию',
+    'sting.base': 'Поле {#label} должно иметь тип sting',
+  },
 
 };
